654f2bc104a7d4870c2fa55da40bb037e8bd2b4e
Dack <598592454@qq.com>
2019-08-08 16:43:50 +0800

    JAVA实现数据结构

---
+++ src/boozs/dack/stack/SqStack.java
@@ -0,0 +1,115 @@
+package boozs.dack.stack;
+
+
+/**
+ * 顺序栈
+ */
+public class SqStack {
+    /**
+     * 栈的长度
+     */
+    private int size;
+    /**
+     * 入栈的元素
+     */
+    private long elem;
+    /**
+     * 栈顶
+     */
+    private int top;
+    /**
+     * 为栈动态分配的数组
+     */
+    private long[] stackArray;
+
+    public SqStack(int size) {
+        this.size = size;
+        this.stackArray = new long[size];
+        // 空栈
+        top = -1;
+    }
+
+    /**
+     * 栈是否已满
+     *
+     * @return
+     */
+    public Boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 是否空栈
+     *
+     * @return
+     */
+    public Boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 入栈
+     *
+     * @param elem
+     */
+    public void push(long elem) {
+        if (isFull()) {
+            System.out.println("栈已满");
+            return;
+        } else {
+            stackArray[++top] = elem;
+        }
+    }
+
+    /**
+     * 出栈
+     */
+    public long pop() {
+        if (isEmpty()) {
+            System.out.println("栈为空");
+            return 0;
+        } else {
+            long elem = stackArray[top];
+            // 删除栈顶元素
+            stackArray[top] = 0;
+            --top;
+            return elem;
+        }
+    }
+
+    /**
+     * 获取栈顶
+     *
+     * @return
+     */
+    public long getTop() {
+        if (isEmpty()) {
+            System.out.println("栈为空");
+            return 0;
+        } else {
+            return stackArray[top];
+        }
+    }
+
+    /**
+     * 打印
+     */
+    public void printStack() {
+        if (isEmpty()) {
+            System.out.println("栈为空");
+            return;
+        } else {
+            for (int i = top; i >= 0; i--) {
+                System.out.println("["+stackArray[i]+"]");
+            }
+        }
+    }
+}
---
+++ src/boozs/dack/stack/StackNode.java
@@ -0,0 +1,29 @@
+package boozs.dack.stack;
+
+/**
+ * 链栈结点
+ */
+public class StackNode {
+    /**
+     * 入栈元素
+     */
+    private long elem;
+
+    private StackNode next;
+
+    public void setElem(long elem) {
+        this.elem = elem;
+    }
+
+    public void setNext(StackNode next) {
+        this.next = next;
+    }
+
+    public long getElem() {
+        return elem;
+    }
+
+    public StackNode getNext() {
+        return next;
+    }
+}
